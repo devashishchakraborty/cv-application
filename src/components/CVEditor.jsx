657406770358bd2eccc7cc6537ff60ffa3c895d4
Devashish Chakraborty <devashishchakra@gmail.com>
2024-10-22 23:14:13 +0530

    Modified the structure of the elements

--- src/components/CVEditor.jsx
+++ src/components/CVEditor.jsx
@@ -1,132 +1,100 @@
 import { useState } from 'react'
 import '../styles/CVEditor.css'
 
-
 export default function CVEditor(){
-    return (
-        <>
-        <div className="cvEditor">
-          <form className='editGeneralInfo'>
-            <fieldset>
-              <legend>General Information</legend>
-              <input type="text" name="fullname" id="fullname" className="fullname" placeholder='Full Name'/>
-              <input type="tel" name="number" id="number" className="number" placeholder='Phone Number'/>
-              <input type="email" name="email" id="email" className="email" placeholder='Email'/>
-              <input type="url" name="linkedin" id="linkedin" className="linkedin" placeholder='LinkedIn Profile'/>
-              <input type="url" name="github" id="github" className="github" placeholder='GitHub Profile'/>
-              <input type="url" name="website" id="website" className="website" placeholder='Personal Website'/>
-              <textarea name="profile" rows="3" placeholder='Profile Description' />
-            </fieldset>
+  const showAddItemForm = (e) => {
+    const form = e.currentTarget.parentElement.nextElementSibling;
+    form.style.display = "grid";
+  }
+  return (
+      <>
+      <div className="cvEditor">
+        <form className='editGeneralInfo'>
+          <fieldset>
+            <legend>General Information</legend>
+            <input type="text" name="fullname" id="fullname" className="fullname" placeholder='Full Name'/>
+            <input type="tel" name="number" id="number" className="number" placeholder='Phone Number'/>
+            <input type="email" name="email" id="email" className="email" placeholder='Email'/>
+            <input type="url" name="linkedin" id="linkedin" className="linkedin" placeholder='LinkedIn Profile'/>
+            <input type="url" name="github" id="github" className="github" placeholder='GitHub Profile'/>
+            <input type="url" name="website" id="website" className="website" placeholder='Personal Website'/>
+            <textarea name="description" id="description" className='description' rows="4" placeholder='Profile Description' />
+          </fieldset>
+        </form>
+
+        <div className='editExperience'>
+          <div className='sectionHeader'>
+            <div className='sectionTitle'>Experience</div>
+            <div className='addItemBtn' onClick={(e) => showAddItemForm(e)}>+</div>
+          </div>
+          <form>
+            <input type="text" name="company" id="company" className="company" placeholder='Company Name'/>
+            <input type="text" name="role" id="role" className="role" placeholder='Role'/>
+            <input type="text" name="location" id="location" className="location" placeholder='Location'/>
+            <div>
+              <label htmlFor="startDate">Start Date: </label>
+              <input type="date" name="startDate" id="startDate" className="startDate"/>
+            </div>
+            <div>
+              <label htmlFor="endDate">End Date: </label>
+              <input type="date" name="endDate" id="endDate" className="endDate"/>
+            </div>
+            <textarea name="description" id="description" rows="4" className='description' placeholder='Description'></textarea>
+            <button className='submitAddItemBtn'>Submit</button>
+            <button className='cancelAddItemBtn'>Cancel</button>
           </form>
-
-          <div className='editExperience'>
-            <div className='sectionHeader'>
-              <div className='sectionTitle'>Experience</div>
-              <div className='addItemBtn'>+</div>
-            </div>
-            <form>
-              <div>
-                <label htmlFor="role">Role: </label>
-                <input type="text" name="role" id="role" className="role"/>
-              </div>
-              <div>
-                <label htmlFor="company">Company Name: </label>
-                <input type="text" name="company" id="company" className="company"/>
-              </div>
-              <div>
-                <label htmlFor="location">Location: </label>
-                <input type="text" name="location" id="location" className="location"/>
-              </div>
-              <div>
-                <label htmlFor="startDate">Start Date: </label>
-                <input type="date" name="startDate" id="startDate" className="startDate"/>
-              </div>
-              <div>
-                <label htmlFor="endDate">End Date: </label>
-                <input type="date" name="endDate" id="endDate" className="endDate"/>
-              </div>
-              <div>
-                <label htmlFor="description">What you worked on? </label>
-                <textarea name="description" id="description"></textarea>
-              </div>
-            </form>
-          </div>
-
-          <div className='editProjects'>
-            <div className="sectionHeader">
-              <div className='sectionTitle'>Projects</div>
-              <div className='addItemBtn'>+</div>
-            </div>
-            <form action="">
-              <div>
-                <label htmlFor="name">Name: </label>
-                <input type="text" id="name" name="name" className="name"/>
-              </div>
-              <div>
-                <label htmlFor="technologiesUsed">Tools/Technologies Used: </label>
-                <input type="text" id="technologiesUsed" name="technologiesUsed" className="technologiesUsed"/>
-              </div>
-              <div>
-                <label htmlFor="description">Description: </label>
-                <textarea name="description" id="description" className="description"></textarea>
-              </div>
-            </form>
-          </div>
-
-          <div className='editEducation'>
-            <div className="sectionHeader">
-              <div className='sectionTitle'>Education</div>
-              <div className='addItemBtn'>+</div>
-            </div>
-            <form action="">
-              <div>
-                <label htmlFor="institutionName">Institution Name: </label>
-                <input type="text" id="institutionName" name="institutionName" className="institutionName"/>
-              </div>
-              <div>
-                <label htmlFor="programmeName">Programme Name: </label>
-                <input type="text" id="programmeName" name="programmeName" className="programmeName"/>
-              </div>
-              <div>
-                <label htmlFor="location">Location: </label>
-                <input type="text" name="location" id="location" className="location"/>
-              </div>
-              <div>
-                <label htmlFor="startDate">Start Date: </label>
-                <input type="date" name="startDate" id="startDate" className="startDate"/>
-              </div>
-              <div>
-                <label htmlFor="endDate">End Date: </label>
-                <input type="date" name="endDate" id="endDate" className="endDate"/>
-              </div>
-            </form>
-          </div>
-          <div className='editSkills'>
-            <div className='sectionHeader'>
-              <div className='sectionTitle'>Technical Skills</div>
-              <div className='addItemBtn'>+</div>
-            </div>
-            <form action="">
-              <div>
-                <label htmlFor="languages">Languages: </label>
-                <input type="text" name="languages" id="languages" className="languages"/>
-              </div>
-              <div>
-                <label htmlFor="frameworks">Frameworks and Libraries: </label>
-                <input type="text" name="frameworks" id="frameworks" className="frameworks"/>
-              </div>
-              <div>
-                <label htmlFor="tools">Development Tools: </label>
-                <input type="text" name="tools" id="tools" className="tools"/>
-              </div>
-              <div>
-                <label htmlFor="certifications">Courses and Certificates: </label>
-                <input type="text" name="certifications" id="certifications" className="certifications"/>
-              </div>
-            </form>
-          </div>
         </div>
 
-        </>
-    )
+        <div className='editProjects'>
+          <div className="sectionHeader">
+            <div className='sectionTitle'>Projects</div>
+            <div className='addItemBtn' onClick={(e) => showAddItemForm(e)}>+</div>
+          </div>
+          <form action="">
+              <input type="text" id="name" name="name" className="name" placeholder='Project Name'/>
+              <input type="text" id="technologiesUsed" name="technologiesUsed" className="technologiesUsed" placeholder='Tools/Technologies Used'/>
+              <textarea name="description" rows="4" id="description" className="description" placeholder='Project Description'></textarea>
+            <button className='submitAddItemBtn'>Submit</button>
+            <button className='cancelAddItemBtn'>Cancel</button>
+          </form>
+        </div>
+
+        <div className='editEducation'>
+          <div className="sectionHeader">
+            <div className='sectionTitle'>Education</div>
+            <div className='addItemBtn' onClick={(e) => showAddItemForm(e)}>+</div>
+          </div>
+          <form action="">
+            <input type="text" id="institutionName" name="institutionName" className="institutionName" placeholder='Institution Name'/>
+            <input type="text" id="programmeName" name="programmeName" className="programmeName" placeholder='Programme Name'/>
+            <input type="text" name="location" id="location" className="location" placeholder='Location'/>
+            <div>
+              <label htmlFor="startDate">Start Date: </label>
+              <input type="date" name="startDate" id="startDate" className="startDate"/>
+            </div>
+            <div>
+              <label htmlFor="endDate">End Date: </label>
+              <input type="date" name="endDate" id="endDate" className="endDate"/>
+            </div>
+            <button className='submitAddItemBtn'>Submit</button>
+            <button className='cancelAddItemBtn'>Cancel</button>
+          </form>
+        </div>
+        <div className='editSkills'>
+          <div className='sectionHeader'>
+            <div className='sectionTitle'>Technical Skills</div>
+            <div className='addItemBtn' onClick={(e) => showAddItemForm(e)}>+</div>
+          </div>
+          <form action="">
+            <input type="text" name="languages" id="languages" className="languages" placeholder='Languages'/>
+            <input type="text" name="frameworks" id="frameworks" className="frameworks" placeholder='Frameworks and Libraries'/>
+            <input type="text" name="tools" id="tools" className="tools" placeholder='Development Tools'/>
+            <input type="text" name="certifications" id="certifications" className="certifications" placeholder='Courses and Certificates'/>
+            <button className='submitAddItemBtn'>Submit</button>
+            <button className='cancelAddItemBtn'>Cancel</button>
+          </form>
+        </div>
+      </div>
+      </>
+  )
 }
