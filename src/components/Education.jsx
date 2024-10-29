2e6040a233dead494a08ea0fe0043fc98740f4ca
Devashish Chakraborty <devashishchakra@gmail.com>
2024-10-30 03:58:46 +0530

    Add sections for display of entries under their respective sections.

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import "../styles/Education.css"
 import { populateYears } from "./CVEditor";
+import { v4 as uuidv4 } from 'uuid';
 
 
 function Education({ activeForm, setActiveForm, educationList, onSubmit }){
   const [newEducation, setNewEducation] = useState({
+    id: uuidv4(),
     institutionName:"",
     programmeName:"",
     location:"",
@@ -14,6 +16,22 @@ function Education({ activeForm, setActiveForm, educationList, onSubmit }){
     endYear:"",
   })
 
+  const [submissionType, setSubmissionType] = useState("Add")
+
+  const clearForm = () => {
+    setNewEducation((prev) => ({
+      ...prev, 
+      companyName:"",
+      role:"",
+      location:"",
+      startMonth:"",
+      startYear:"",
+      endMonth:"",
+      endYear:"",
+      description:""
+    }))
+  }
+
   const handleChange = (e) => {
     const { name, value } = e.target;
     console.log(name, value)
@@ -21,31 +39,30 @@ function Education({ activeForm, setActiveForm, educationList, onSubmit }){
   }
 
   const handleClear = () => {
-    setActiveForm("")
-    setNewEducation({
-      institutionName:"",
-      programmeName:"",
-      location:"",
-      startMonth:"",
-      startYear:"",
-      endMonth:"",
-      endYear:"",
-    })
+    setActiveForm("");
+    clearForm();
   }
 
   const handleSubmit = (e) => {
-    e.preventDefault()
-    onSubmit(newExperience)
-    setNewEducation({
-      institutionName:"",
-      programmeName:"",
-      location:"",
-      startMonth:"",
-      startYear:"",
-      endMonth:"",
-      endYear:"",
-    })
+    e.preventDefault();
+    onSubmit(newEducation);
+    setActiveForm("");
+    clearForm();
   }
+
+  const handleAddItem = () => {
+    setSubmissionType("Add");
+    clearForm();
+    setActiveForm("editEducation");
+  }
+
+  const handleEditItem = (id) => {
+    setSubmissionType("Edit");
+    const experienceToEdit = experienceList.find(experience => experience.id === id)
+    setNewExperience(experienceToEdit);
+    setActiveForm("editExperience");
+  }
+
 
   return (
       <div className='editEducation'>
@@ -54,7 +71,7 @@ function Education({ activeForm, setActiveForm, educationList, onSubmit }){
         <svg xmlns="http://www.w3.org/2000/svg" width="1.2em" height="1.2em" viewBox="0 0 24 24"><rect width="24" height="24" fill="none"/><path fill="currentColor" d="M12 3L1 9l4 2.18v6L12 21l7-3.82v-6l2-1.09V17h2V9zm6.82 6L12 12.72L5.18 9L12 5.28zM17 16l-5 2.72L7 16v-3.73L12 15l5-2.73z"/></svg>
           Education
           </div>
-        <div className='addItemBtn' onClick={() => setActiveForm('editEducation')}>+</div>
+        <div className='addItemBtn' onClick={handleAddItem}>+</div>
       </div>
       {
         activeForm === 'editEducation' ? (
@@ -64,8 +81,8 @@ function Education({ activeForm, setActiveForm, educationList, onSubmit }){
           <input type="text" name="location" id="location" className="location" placeholder='Location' onChange={(e)=>handleChange(e)}/>
           <div className="startDate">
             Start Date:
-            <select id="startMonth" name="startMonth" onChange={(e)=>handleChange(e)}>
-              <option value="" disabled selected hidden>Month</option>
+            <select id="startMonth" name="startMonth" defaultValue={newEducation.startMonth.length > 0 ? newEducation.startMonth : "Month"} onChange={(e)=>handleChange(e)} required>
+              <option disabled hidden>Month</option>
               <option>January</option>
               <option>February</option>
               <option>March</option>
@@ -79,15 +96,15 @@ function Education({ activeForm, setActiveForm, educationList, onSubmit }){
               <option>November</option>
               <option>December</option>
             </select>
-            <select id="startYear" name="startYear" onChange={(e)=>handleChange(e)}>
-              <option value="" disabled selected hidden>Year</option>
+            <select id="startYear" name="startYear" defaultValue={newEducation.startYear.length > 0 ? newEducation.startYear : "Year"} onChange={(e)=>handleChange(e)} required>
+              <option disabled hidden>Year</option>
               { populateYears() }
             </select>
           </div>
           <div className="endDate">
             End Date:
-            <select id="endMonth" name="endMonth" onChange={(e)=>handleChange(e)}>
-              <option value="" disabled selected hidden>Month</option>
+            <select id="endMonth" name="endMonth" defaultValue={newEducation.endMonth.length > 0 ? newEducation.endMonth : "Month"} onChange={(e)=>handleChange(e)} required>
+              <option disabled hidden>Month</option>
               <option>January</option>
               <option>February</option>
               <option>March</option>
@@ -101,16 +118,33 @@ function Education({ activeForm, setActiveForm, educationList, onSubmit }){
               <option>November</option>
               <option>December</option>
             </select>
-            <select id="endYear" name="endYear" onChange={(e)=>handleChange(e)}>
-              <option value="" disabled selected hidden>Year</option>
+            <select id="endYear" name="endYear" defaultValue={newEducation.endYear.length > 0 ? newEducation.endYear : "Year"} onChange={(e)=>handleChange(e)} required>
+              <option disabled hidden>Year</option>
               { populateYears() }
             </select>
           </div>
           <button className='submitAddItemBtn' type="submit">Add</button>
-          <button className='cancelAddItemBtn' type='reset' onClick={handleClear}>Cancel</button>
+          <button className='cancelAddItemBtn' type='reset' onClick={(e) => handleClear(e)}>Cancel</button>
         </form>  
-        ) : null
-      }
+        ) : (educationList.length > 0 ? (
+          <div className="educationList">
+            {
+              educationList.map((education) => {
+                return (
+                  <div className="educationDetails">
+                    <div className="items">
+                      <div className="institutionName">{education.institutionName}</div>
+                      <div className="programmeName">{education.programmeName}</div>
+                      <div className="location">{education.location}</div>
+                    </div>
+                    <div className="editBtn" onClick={() => handleEditItem(experience.id)}><svg xmlns="http://www.w3.org/2000/svg" width="1.6em" height="1.6em" viewBox="0 0 24 24"><rect width="24" height="24" fill="none"/><path fill="currentColor" d="m14.06 9l.94.94L5.92 19H5v-.92zm3.6-6c-.25 0-.51.1-.7.29l-1.83 1.83l3.75 3.75l1.83-1.83c.39-.39.39-1.04 0-1.41l-2.34-2.34c-.2-.2-.45-.29-.71-.29m-3.6 3.19L3 17.25V21h3.75L17.81 9.94z"/></svg></div>
+                    <div className="deleteBtn"><svg xmlns="http://www.w3.org/2000/svg" width="1.6em" height="1.6em" viewBox="0 0 24 24"><rect width="24" height="24" fill="none"/><path fill="currentColor" d="M6 19a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V7H6zM8 9h8v10H8zm7.5-5l-1-1h-5l-1 1H5v2h14V4z"/></svg></div>
+                  </div>
+                )
+              })
+            }
+          </div>
+        ) : null)}
     </div>
   )
 }
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,36 +1,40 @@
 import { useState } from "react";
-import "../styles/Projects.css"
+import "../styles/Projects.css";
+import { v4 as uuidv4 } from 'uuid';
 
 
-function Projects({ activeForm, setActiveForm, projectsList, onSubmit }){
+
+function Projects({ activeForm, setActiveForm, projectsList, onSubmit }) {
   const [newProject, setNewProject] = useState({
-    name:"",
-    technologiesUsed:"",
-    description:""
+    id: uuidv4(),
+    name: "",
+    technologiesUsed: "",
+    description: ""
   })
 
   const handleChange = (e) => {
     const { name, value } = e.target;
     console.log(name, value)
-    setNewProject((prev) => ({...prev, [name]:value }))
+    setNewProject((prev) => ({ ...prev, [name]: value }))
   }
 
   const handleClear = () => {
     setActiveForm("")
     setNewProject({
-      name:"",
-      technologiesUsed:"",
-      description:""
+      name: "",
+      technologiesUsed: "",
+      description: ""
     })
   }
 
   const handleSubmit = (e) => {
-    e.preventDefault()
-    onSubmit(newExperience)
+    e.preventDefault();
+    onSubmit(newProject);
+    setActiveForm("");
     setNewProject({
-      name:"",
-      technologiesUsed:"",
-      description:""
+      name: "",
+      technologiesUsed: "",
+      description: ""
     })
   }
 
@@ -38,8 +42,8 @@ function Projects({ activeForm, setActiveForm, projectsList, onSubmit }){
     <div className='editProjects'>
       <div className="sectionHeader">
         <div className='sectionTitle'>
-        <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 16 16"><rect width="16" height="16" fill="none"/><path fill="currentColor" d="M4.75 7a.75.75 0 0 0 0 1.5h4.5a.75.75 0 0 0 0-1.5zM5 4.75A.75.75 0 0 1 5.75 4h5.5a.75.75 0 0 1 0 1.5h-5.5A.75.75 0 0 1 5 4.75M6.75 10a.75.75 0 0 0 0 1.5h4.5a.75.75 0 0 0 0-1.5z"/><path fill="currentColor" d="M0 1.75C0 .784.784 0 1.75 0h12.5C15.216 0 16 .784 16 1.75v12.5A1.75 1.75 0 0 1 14.25 16H1.75A1.75 1.75 0 0 1 0 14.25Zm1.75-.25a.25.25 0 0 0-.25.25v12.5c0 .138.112.25.25.25h12.5a.25.25 0 0 0 .25-.25V1.75a.25.25 0 0 0-.25-.25Z"/></svg>
-            Projects
+          <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 16 16"><rect width="16" height="16" fill="none" /><path fill="currentColor" d="M4.75 7a.75.75 0 0 0 0 1.5h4.5a.75.75 0 0 0 0-1.5zM5 4.75A.75.75 0 0 1 5.75 4h5.5a.75.75 0 0 1 0 1.5h-5.5A.75.75 0 0 1 5 4.75M6.75 10a.75.75 0 0 0 0 1.5h4.5a.75.75 0 0 0 0-1.5z" /><path fill="currentColor" d="M0 1.75C0 .784.784 0 1.75 0h12.5C15.216 0 16 .784 16 1.75v12.5A1.75 1.75 0 0 1 14.25 16H1.75A1.75 1.75 0 0 1 0 14.25Zm1.75-.25a.25.25 0 0 0-.25.25v12.5c0 .138.112.25.25.25h12.5a.25.25 0 0 0 .25-.25V1.75a.25.25 0 0 0-.25-.25Z" /></svg>
+          Projects
         </div>
         <div className='addItemBtn' onClick={() => setActiveForm("editProjects")}>+</div>
       </div>
@@ -47,13 +51,27 @@ function Projects({ activeForm, setActiveForm, projectsList, onSubmit }){
       {
         activeForm === "editProjects" ? (
           <form onSubmit={(e) => handleSubmit(e)}>
-            <input type="text" id="name" name="name" className="name" placeholder='Project Name' onChange={(e) => handleChange(e)}/>
-            <input type="text" id="technologiesUsed" name="technologiesUsed" className="technologiesUsed" placeholder='Tools/Technologies Used' onChange={(e) => handleChange(e)}/>
-            <textarea name="description" rows="4" id="description" className="description" placeholder='Project Description' onChange={(e) => handleChange(e)}></textarea>
+            <input type="text" id="name" name="name" className="name" placeholder='Project Name' onChange={(e) => handleChange(e)} required />
+            <input type="text" id="technologiesUsed" name="technologiesUsed" className="technologiesUsed" placeholder='Tools/Technologies Used' onChange={(e) => handleChange(e)} required />
+            <textarea name="description" rows="4" id="description" className="description" placeholder='Project Description' onChange={(e) => handleChange(e)} required></textarea>
             <button className='submitAddItemBtn' type="submit">Add</button>
-            <button className='cancelAddItemBtn' type='reset' onClick={handleClear}>Cancel</button>
+            <button className='cancelAddItemBtn' type='reset' onClick={(e) => handleClear(e)}>Cancel</button>
           </form>
-        ) : null
+        ) : (projectsList.length > 0 ? (
+          <div className="projectsList">
+            {
+              projectsList.map((project) => {
+                return (
+                  <div className="projectDetails">
+                    <div className="name">{project.name}</div>
+                    <div className="technologiesUsed">{project.technologiesUsed}</div>
+                    <div className="description">{project.description}</div>
+                  </div>
+                )
+              })
+            }
+          </div>
+        ) : null)
       }
     </div>
   )
