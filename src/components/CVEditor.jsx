2b5dde3f99e214f9ba54a6a8dd5297c2665592b5
Devashish Chakraborty <devashishchakra@gmail.com>
2024-10-30 03:59:56 +0530

    Modified the functions for form submission to handle entries where the submission type is edit an existing entry

--- src/components/CVEditor.jsx
+++ src/components/CVEditor.jsx
@@ -13,27 +13,27 @@ const populateYears = () => {
   const years = [];
 
   // Make this year, and the 100 years before it available in the year <select>
-  for (let i = 0; i <= 100; i++) years.push(<option key={i}>{currentYear - i}</option>)
+  for (let i = 0; i <= 50; i++) years.push(<option key={i}>{currentYear - i}</option>)
   return years;
 }
 
-function CVEditor(){
+function CVEditor() {
   const [activeForm, setActiveForm] = useState("")
   const [generalInfo, setGeneralInfo] = useState({
-    fullname:"",
-    number:"",
-    email:"",
-    linkedin:"",
-    github:"",
-    website:"",
-    description:""
+    fullname: "",
+    number: "",
+    email: "",
+    linkedin: "",
+    github: "",
+    website: "",
+    description: ""
   })
 
   const [skills, setSkills] = useState({
-    languages:"",
-    frameworks:"",
-    tools:"",
-    certifications:""
+    languages: "",
+    frameworks: "",
+    tools: "",
+    certifications: ""
   })
 
   const [experience, setExperience] = useState([])
@@ -41,38 +41,50 @@ function CVEditor(){
   const [education, setEducation] = useState([])
 
 
-  function handleGeneralInfoChange(e){
+  function handleGeneralInfoChange(e) {
     const { name, value } = e.target;
-    setGeneralInfo((prev) => ({...prev, [name]:value }))
+    setGeneralInfo((prev) => ({ ...prev, [name]: value }))
   }
 
-  function handleSkillsChange(e){
-    const {name, value} = e.target;
-    setSkills((prev) => ({...prev, [name]:value}))
+  function handleSkillsChange(e) {
+    const { name, value } = e.target;
+    setSkills((prev) => ({ ...prev, [name]: value }))
   }
 
-  function handleExperienceSubmit(newExperience){
-    setExperience((prev) => [...prev, newExperience])
+  function handleExperienceSubmit(newJob, submissionType) {
+    setExperience((prev) => {
+      if (submissionType === "Edit") {
+        return prev.map(job => (job.id === newJob.id ? newJob : job))
+      } else return [...prev, newJob]
+    })
   }
 
-  function handleEducationSubmit(newEducation){
-    setEducation((prev) => [...prev, newEducation])
+  function handleEducationSubmit(newEducation, submissionType) {
+    setEducation((prev) => {
+      if (submissionType === "Edit") {
+        return prev.map(education => (education.id === newEducation.id ? newEducation : education))
+      } else return [...prev, newEducation]
+    })
   }
 
-  function handleProjectsSubmit(newProject){
-    setProjects((prev) => [...prev, newProject])
+  function handleProjectsSubmit(newProject, submissionType) {
+    setProjects((prev) => {
+      if (submissionType === "Edit") {
+        return prev.map(project => (project.id === newProject.id ? newProject : project))
+      } else return [...prev, newProject]
+    })
   }
 
   return (
-      <>
+    <>
       <div className="cvEditor">
-        <GeneralInfo generalInfoItems={generalInfo} handleChange={handleGeneralInfoChange}/>
-        <Skills skillsItems={skills} handleChange={handleSkillsChange}/>
-        <Experience activeForm={activeForm} setActiveForm={setActiveForm} experienceList={experience} onSubmit={handleExperienceSubmit}/>
-        <Projects activeForm={activeForm} setActiveForm={setActiveForm} projectsList={projects} onSubmit={handleProjectsSubmit}/>
-        <Education activeForm={activeForm} setActiveForm={setActiveForm} educationList={education} onSubmit={handleEducationSubmit}/>
+        <GeneralInfo generalInfoItems={generalInfo} handleChange={handleGeneralInfoChange} />
+        <Skills skillsItems={skills} handleChange={handleSkillsChange} />
+        <Experience activeForm={activeForm} setActiveForm={setActiveForm} experienceList={experience} onSubmit={handleExperienceSubmit} />
+        <Projects activeForm={activeForm} setActiveForm={setActiveForm} projectsList={projects} onSubmit={handleProjectsSubmit} />
+        <Education activeForm={activeForm} setActiveForm={setActiveForm} educationList={education} onSubmit={handleEducationSubmit} />
       </div>
-      </>
+    </>
   )
 }
 
