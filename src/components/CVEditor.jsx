609d33240f7f26e210eecccc61be074536167c96
Devashish Chakraborty <devashishchakra@gmail.com>
2024-10-30 20:04:31 +0530

    Added functions for handling item delete action. Also passed the PopulateYears as prop to other components

--- src/components/CVEditor.jsx
+++ src/components/CVEditor.jsx
@@ -6,16 +6,6 @@ import Projects from './Projects';
 import Education from './Education';
 import Skills from './Skills';
 
-const populateYears = () => {
-  // Get the current year as a number
-  const date = new Date();
-  const currentYear = date.getFullYear();
-  const years = [];
-
-  // Make this year, and the 100 years before it available in the year <select>
-  for (let i = 0; i <= 50; i++) years.push(<option key={i}>{currentYear - i}</option>)
-  return years;
-}
 
 function CVEditor() {
   const [activeForm, setActiveForm] = useState("")
@@ -36,11 +26,24 @@ function CVEditor() {
     certifications: ""
   })
 
+  // Populating Years for Item submit form
+  const PopulateYears = () => {
+    // Get the current year as a number
+    const date = new Date();
+    const currentYear = date.getFullYear();
+    const years = [];
+
+    // Make this year, and the 100 years before it available in the year <select>
+    for (let i = 0; i <= 50; i++) years.push(<option key={i}>{currentYear - i}</option>)
+    return years;
+  }
+
+
   const [experience, setExperience] = useState([])
   const [projects, setProjects] = useState([])
   const [education, setEducation] = useState([])
 
-
+  // Sections involving real time data input
   function handleGeneralInfoChange(e) {
     const { name, value } = e.target;
     setGeneralInfo((prev) => ({ ...prev, [name]: value }))
@@ -51,42 +54,82 @@ function CVEditor() {
     setSkills((prev) => ({ ...prev, [name]: value }))
   }
 
+  // Callback function to return updated list after adding new Item
+  function updatedList(prev, newItem, submissionType) {
+    if (submissionType === "Edit") {
+      return prev.map(item => (item.id === newItem.id ? newItem : item))
+    } else return [...prev, newItem]
+  }
+
+  // Form Submission Handlers
   function handleExperienceSubmit(newJob, submissionType) {
-    setExperience((prev) => {
-      if (submissionType === "Edit") {
-        return prev.map(job => (job.id === newJob.id ? newJob : job))
-      } else return [...prev, newJob]
-    })
+    setExperience((prev) => updatedList(prev, newJob, submissionType))
   }
 
   function handleEducationSubmit(newEducation, submissionType) {
-    setEducation((prev) => {
-      if (submissionType === "Edit") {
-        return prev.map(education => (education.id === newEducation.id ? newEducation : education))
-      } else return [...prev, newEducation]
-    })
+    setEducation((prev) => updatedList(prev, newEducation, submissionType))
   }
 
-  function handleProjectsSubmit(newProject, submissionType) {
-    setProjects((prev) => {
-      if (submissionType === "Edit") {
-        return prev.map(project => (project.id === newProject.id ? newProject : project))
-      } else return [...prev, newProject]
-    })
+  function handleProjectSubmit(newProject, submissionType) {
+    setProjects((prev) => updatedList(prev, newProject, submissionType))
   }
+
+  // handle deletion of items from forms
+  function handleExperienceDelete(id) {
+    setExperience((prev) => prev.filter((job) => job.id != id))
+  }
+
+  function handleEducationDelete(id) {
+    setEducation((prev) => prev.filter((education) => education.id != id))
+  }
+
+  function handleProjectDelete(id) {
+    setProjects((prev) => prev.filter((project) => project.id != id))
+  }
+
 
   return (
     <>
       <div className="cvEditor">
-        <GeneralInfo generalInfoItems={generalInfo} handleChange={handleGeneralInfoChange} />
-        <Skills skillsItems={skills} handleChange={handleSkillsChange} />
-        <Experience activeForm={activeForm} setActiveForm={setActiveForm} experienceList={experience} onSubmit={handleExperienceSubmit} />
-        <Projects activeForm={activeForm} setActiveForm={setActiveForm} projectsList={projects} onSubmit={handleProjectsSubmit} />
-        <Education activeForm={activeForm} setActiveForm={setActiveForm} educationList={education} onSubmit={handleEducationSubmit} />
+        <GeneralInfo
+          generalInfoItems={generalInfo}
+          handleChange={handleGeneralInfoChange}
+        />
+        
+        <Skills 
+          skillsItems={skills} 
+          handleChange={handleSkillsChange} 
+        />
+
+        <Experience 
+          activeForm={activeForm} 
+          setActiveForm={setActiveForm} 
+          experienceList={experience}
+          PopulateYears={PopulateYears}
+          onSubmit={handleExperienceSubmit}
+          onDelete={handleExperienceDelete}
+        />
+
+        <Projects 
+          activeForm={activeForm} 
+          setActiveForm={setActiveForm} 
+          projectsList={projects}
+          onSubmit={handleProjectSubmit} 
+          onDelete={handleProjectDelete}
+        />
+
+        <Education 
+          activeForm={activeForm} 
+          setActiveForm={setActiveForm} 
+          educationList={education}
+          PopulateYears={PopulateYears}
+          onSubmit={handleEducationSubmit} 
+          onDelete={handleEducationDelete}
+        />
       </div>
     </>
   )
 }
 
 
-export { CVEditor, populateYears }
+export default CVEditor;
